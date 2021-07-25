c784066737f16e729fd02ccf620c3960bd083ec7
Sumit Yadav <41279119+ersumityadav@users.noreply.github.com>
2021-07-25 08:37:55 +0530

    Add files via upload

---
+++ models/post.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose');
+
+
+const postSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    //include the array of id's of all comments in this schema itself
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},{
+        timestamps: true
+});
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
